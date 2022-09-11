1626ff7fd731f3d311ea90022daaa24086de6427
Grigoriy Efimov <efimov-gm@newpage.xyz>
2022-09-11 20:25:08 +0300

    Improve security

--- src/app/Http/Controllers/ReplController.php
+++ src/app/Http/Controllers/ReplController.php
@@ -40,7 +40,7 @@ class ReplController extends Controller
         $copyCode = "cd {$pathToPythonTmpDir} ; sshpass -p \"123\" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {$tmpFileName}  root@sandbox:/app/{$tmpName}/main.py";
         shell_exec($copyCode);
 
-        $sandboxCode = 'sshpass -p "123" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@sandbox docker run -i --rm --name pythonsandbox -v /app/'.$tmpName.':/usr/src/myapp -w /usr/src/myapp python:3 python main.py  2>&1';
+        $sandboxCode = 'sshpass -p "123" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@sandbox docker run -i --network none --rm --name pythonsandbox -v /app/'.$tmpName.':/usr/src/myapp -w /usr/src/myapp python:3 python main.py  2>&1';
         $output = shell_exec($sandboxCode);
 
         $rmCode = "sshpass -p \"123\" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@sandbox rm -R /app/{$tmpName} ";
