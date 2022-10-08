62010544ca8c6d2c2039e71b98792d5ee68aceb8
Grigoriy Efimov <efimov-gm@newpage.xyz>
2022-10-08 13:54:50 +0300

    Add autopulling python in dind

--- docker/sandbox/entrypoint.sh
+++ docker/sandbox/entrypoint.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+#Start sshd 
 /usr/sbin/sshd -D &
 
-dockerd
+# Turn on bash's job control
+set -m
+
+# Start docker service in background
+/usr/local/bin/dockerd-entrypoint.sh &
+
+# Wait that the docker service is up
+while ! docker info; do
+  echo "Waiting docker..."
+  sleep 3
+done
+
+# Import pre-installed images
+FILE=/usr/src/app/python3.tar.gz
+
+if test -f "$PYTON3FILE"; then
+    docker load < "$PYTON3FILE"
+else 
+    docker pull python:3
+    docker save python:3 > "$PYTON3FILE"
+fi
+
+# Bring docker service back to foreground
+fg %2
