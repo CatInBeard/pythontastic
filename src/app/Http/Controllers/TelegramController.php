2eca7ff7ec693ef8e29b40b82a103ef8fd31daea
Grigoriy Efimov <efimov-gm@newpage.xyz>
2022-10-25 23:00:16 +0300

    Add telegram header checking

--- src/app/Http/Controllers/TelegramController.php
+++ src/app/Http/Controllers/TelegramController.php
@@ -9,6 +9,12 @@ class TelegramController extends Controller
 {
     function receiveWebhook(Request $request)
     {
+        $telegramSecret = $request->header('X-Telegram-Bot-Api-Secret-Token');
+
+        if($telegramSecret != config('app.telegram_bot_secret_token')){
+            abort(403);
+        }
+
         $Telegram = new TelegramRepository();
         if ($request->isJson()) {
             $data = $request->json()->all();
